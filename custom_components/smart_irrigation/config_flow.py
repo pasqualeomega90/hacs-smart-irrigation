c3d8f44c1be68f83d523d12dcdf983e817c3cb11
pasqualeomega90 <pasquale.omega@gmail.com>
2025-09-06 14:57:13 +0200

    Aggiunta selezione dispositivi per zone tramite UI

--- custom_components/smart_irrigation/config_flow.py
+++ custom_components/smart_irrigation/config_flow.py
@@ -1,30 +1,37 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.helpers.entity_registry import async_get
 from .const import DOMAIN
-
-DATA_SCHEMA = vol.Schema({
-    vol.Required("name", default="Irrigazione Smart MultiZona"): str,
-    vol.Required("zone_count", default=2): int,
-    vol.Required("durata_zona_1", default=10): int,
-    vol.Required("durata_zona_2", default=10): int,
-    vol.Required("modalita_zona_1", default="automatica"): vol.In(["manuale", "automatica"]),
-    vol.Required("modalita_zona_2", default="automatica"): vol.In(["manuale", "automatica"]),
-    vol.Required("orario_zona_1", default="alba"): vol.In(["alba", "tramonto"]),
-    vol.Required("offset_zona_1", default=0): int,
-    vol.Required("orario_zona_2", default="alba"): vol.In(["alba", "tramonto"]),
-    vol.Required("offset_zona_2", default=0): int,
-})
 
 class SmartIrrigationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Gestione del flusso di configurazione tramite UI."""
 
     async def async_step_user(self, user_input=None):
         errors = {}
+        entity_registry = async_get(self.hass)
+        # Filtra solo switch e valve
+        entities = [
+            e.entity_id for e in entity_registry.entities.values()
+            if e.domain in ["switch", "valve"]
+        ]
+        schema = vol.Schema({
+            vol.Required("name", default="Irrigazione Smart MultiZona"): str,
+            vol.Required("zone_1_entity"): vol.In(entities),
+            vol.Required("zone_2_entity"): vol.In(entities),
+            vol.Required("durata_zona_1", default=10): int,
+            vol.Required("durata_zona_2", default=10): int,
+            vol.Required("modalita_zona_1", default="automatica"): vol.In(["manuale", "automatica"]),
+            vol.Required("modalita_zona_2", default="automatica"): vol.In(["manuale", "automatica"]),
+            vol.Required("orario_zona_1", default="alba"): vol.In(["alba", "tramonto"]),
+            vol.Required("offset_zona_1", default=0): int,
+            vol.Required("orario_zona_2", default="alba"): vol.In(["alba", "tramonto"]),
+            vol.Required("offset_zona_2", default=0): int,
+        })
         if user_input is not None:
             return self.async_create_entry(title=user_input["name"], data=user_input)
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=schema, errors=errors
         )
 
     @staticmethod
@@ -37,11 +44,27 @@ class SmartIrrigationOptionsFlowHandler(config_entries.OptionsFlow):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        errors = {}
+        entity_registry = async_get(self.hass)
+        entities = [
+            e.entity_id for e in entity_registry.entities.values()
+            if e.domain in ["switch", "valve"]
+        ]
+        schema = vol.Schema({
+            vol.Required("zone_1_entity"): vol.In(entities),
+            vol.Required("zone_2_entity"): vol.In(entities),
+            vol.Required("durata_zona_1", default=10): int,
+            vol.Required("durata_zona_2", default=10): int,
+            vol.Required("modalita_zona_1", default="automatica"): vol.In(["manuale", "automatica"]),
+            vol.Required("modalita_zona_2", default="automatica"): vol.In(["manuale", "automatica"]),
+            vol.Required("orario_zona_1", default="alba"): vol.In(["alba", "tramonto"]),
+            vol.Required("offset_zona_1", default=0): int,
+            vol.Required("orario_zona_2", default="alba"): vol.In(["alba", "tramonto"]),
+            vol.Required("offset_zona_2", default=0): int,
+        })
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
         return self.async_show_form(
             step_id="init",
-            data_schema=DATA_SCHEMA,
-            errors=errors,
+            data_schema=schema,
+            errors={},
         )
